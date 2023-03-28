ed2aae2e07ce32c5ad7cde4ae0847029ab7696ab
p-rsanchez <p-rsanchez@P-RSANCHEZ.neoris.cxnetworks.net>
2023-03-28 10:19:09 +0200

    Ejercicios actualizados

--- Ejercicios/src/apartado3Ejercicio2/DatosArray.java
+++ Ejercicios/src/apartado3Ejercicio2/DatosArray.java
@@ -2,7 +2,29 @@ package apartado3Ejercicio2;
 
 public class DatosArray {
 	
-	int[] diasMes = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	int[][] mes = new int[5][7];
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] diasMes = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		int[][] mes = new int[5][7];
+		
+		int contador = 0;
+		
+		for(int i = 0; i < mes.length; i++) {
+			for(int j = 0; j < mes[i].length; j++){
+				if(contador <= 31) {
+					mes[i][j] = contador++;
+				}
+			}
+		}
+		
+		for(int i = 0; i < mes.length; i++) {
+			for(int j = 0; j < mes[i].length; j++){
+				System.out.println(mes[i][j]);
+			}
+		}
+		
+		
+	}
 	
 }
